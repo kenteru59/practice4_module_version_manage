77c93cfa767886ca533c9f4c37d2138ce3a099ba
k-abe <f.kaebnet.o.cus@gmail.com>
2025-03-24 00:06:25 +0900

    Made some changes to webserver-cluster

--- services/webserver-cluster/user-data.sh
+++ services/webserver-cluster/user-data.sh
@@ -5,6 +5,7 @@ cat > index.html <<EOF
 <p>DB address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
 <p>こちらは ${env_name} 環境です。</p>
+<p>こちらはv0.0.2なんです！</p>
 EOF
 
 nohup busybox httpd -f -p ${server_port} &
